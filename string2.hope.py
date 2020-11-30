aa9a8b99764a7138adaeb06781b92036a4339d4c
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-11-30 18:25:49 +0300

    Add files via upload

---
+++ string2.hope.py
@@ -0,0 +1,4 @@
+nume = str(input("Cum te numesti?\n"))
+varsta = str(input("Cati ani ai?\n"))
+hobby = str(input("Care este hobby-ul tau?\n"))
+print(f'Ma numesc {nume}, am {varsta} ani, hobby-ul meu este {hobby}', end="." )
